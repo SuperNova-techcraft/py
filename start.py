5a11b935c6157bc1e15c2837f894e83294dde480
SuperNova-techcraf <compitoscuolanofobia@gmail.com>
2026-08-07 21:23:51 +0200

    the basic game

--- start.py
+++ start.py
@@ -1,1 +1,14 @@
-print("hello git!!!")
+import random
+ran = int(random.randint(0, 1000))
+def n():
+    dij = int(input())
+    if(dij < ran):
+        print("the number is bigger")
+        n()
+    if(dij > ran):
+        print("the number is littler")
+        n()
+    if(dij == ran):
+        print("you won")
+    dij = int(input())
+n()
