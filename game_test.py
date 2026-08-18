4712ad1ba16d4ddca95619c339b340a90a1a3e7a
SuperNova-techcraf <compitoscuolanofobia@gmail.com>
2026-08-18 10:03:24 +0200

    adding the dash(work in progress) and a bkg

--- game_test.py
+++ game_test.py
@@ -1,30 +1,86 @@
 import pygame
+import time
 from sys import exit
 
-
+bkg = pygame.image.load("img/bkg.png")
 pygame.init()
 window = pygame.display.set_mode((1000, 600))  #window size
+
+
 pygame.display.set_caption("SN-  PyGame") #name on upbar
 clock = pygame.time.Clock()
 #(from x, from y, lumghezza, larcghezza)
-player = pygame.Rect(200, 300, 20, 25)
-
+player = pygame.Rect(200, 300, 20, 25) #player quando punta sopra o sotto
+player_l = pygame.Rect(200, 300, 25, 20) #player_l quando punta a estra o sinistra
+s_l = False #verifica che il personaggio guarda a sinistra
+s_u = False # verifica che il personaggio guarda sopra
+ss_l = False #verifica che lultimo input era a o d
+ss_u = False #verifica che lultimo input era w o s
+s_done = False #verifica se il giocatore ha sprintato
 while True:
     window.fill("#15191f")
-    pygame.draw.rect(window, "red", player)
+    window.blit(bkg, (0, 0))
+    if(ss_l == False):
+        pygame.draw.rect(window, "red", player)
+    else:
+        pygame.draw.rect(window, "red", player_l)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  #press the close button
             pygame.quit()
             exit()
+
+
+#sprint
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_e:
+            if (ss_l == True):
+                if(s_l == True):
+                    if(s_done == False):
+                        player.x -= 100
+                        player_l.x -= 100
+                        s_done = True
+                if(s_l == False):
+                    if(s_done == False):
+                        player.x += 100
+                        player_l.x += 100
+                        time.sleep(1)
+                        s_done = True
+        if event.key == pygame.K.q:
+            s_done = False
+
+#muovimenti
     kes = pygame.key.get_pressed()
-    if kes[pygame.K_w]:
-        player .y -= 5
-    if kes[pygame.K_s]:
-            player .y += 5
-    if kes[pygame.K_a]:
-            player .x -= 5
-    if kes[pygame.K_d]:
-            player .x += 5
+    if kes[pygame.K_w] and (player and player_l).y - 10 >= 0 :
+        player .y -= 10
+        player_l .y -= 10
+        ss_l = False
+        ss_u = True
+        s_u = True
+        # il personaggio sta guardando sopra e l'ultimo input era w
+    if kes[pygame.K_s] and (player and player_l).y + 10 + 25 <= 600:
+        player .y += 10
+        player_l .y += 10
+        ss_l = False
+        ss_u = True
+        s_u = False
+        # il personaggio sta guardando sotto e l'ultimo input era s
+    if kes[pygame.K_a] and (player and player_l).x - 10 >= 0:
+        player_l .x -= 10
+        player .x -= 10
+        ss_l = True
+        ss_u = False
+        s_l = True
+        # il personaggio sta guardando a destra o a sinistra e l'ultimo input era a
+    if kes[pygame.K_d] and (player and player_l).x + 10 + 25 <= 1000:
+        player_l .x += 10
+        player .x += 10
+        ss_l = True
+        ss_u = False
+        s_l = False
+        # il personaggio sta guardando a destra o a sinistra e l'ultimo input era d
+
+
+
 
     pygame.display.update()
     clock.tick(60)  #fps
